b46c93edc9aa4c8bf84d9590e0efc50cf215a52b
developer15 <support@d-logic.rs>
2019-12-21 18:08:57 +0100

    License details added in root folder, and modificated files

--- config.h
+++ config.h
@@ -1,3 +1,37 @@
+/*
+ * Version modified for use of Fluidsynth with iOS
+ * Copyright (C) {2019} {Digital Logic LTD}
+ *
+ * This library is free software; you can
+ * redistribute it and/or
+ * modify it under the terms of the GNU Lesser
+ * General Public
+ * License as published by the Free Software
+ * Foundation; either
+ * version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+
+ * You should have received a copy of the GNU
+ * Lesser General Public License along with
+ * this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin
+ * Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * You can contact representatives of
+ * Digital Logic by e-mail 
+ * g2ames.developer@gmail.com
+ * or you can write to us to address:
+ * Digital Logic LTD, Nemanjina 57A, Pozarevac,
+ * Serbia.
+ */
+
 /* FluidSynth - A Software Synthesizer
  *
  * Copyright (C) 2003  Peter Hanappe and others.
